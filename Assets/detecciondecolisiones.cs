3ce63646c3d9a76494323a42e098cb3edb3744b8
Noah-Marabi <noahmarab@gmail.com>
2025-05-22 10:25:19 -0300

    contacto

---
+++ Assets/detecciondecolisiones.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detecciondecolisiones : MonoBehaviour
+{
+    void OnCollisionEnter()
+    {
+        Debug.Log("Contacto");
+        Destroy(gameObject);
+    }
+}
